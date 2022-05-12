ea387fa44aa4c512bda5f7f608b0c0fe42a8f924
sushikim <omakasekim@naver.com>
2022-05-12 16:40:00 +0900

    java constructor

---
+++ Java/Constructor/Constructor/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Constructor =     special method that is called when an object is instantiated 
+    Human human1 = new Human("Rick", 65, 65.0);
+    Human human2 = new Human("morty", 16, 65.0);
+
+}
+}
+
