1b0b173d98d3858e4f986799be9afaabd4d48e2a
sushikim <omakasekim@naver.com>
2022-07-11 18:38:16 +0900

    updated commits

--- AlgAndDS/Eratosthenes/sieve.cpp
+++ AlgAndDS/Eratosthenes/sieve.cpp
@@ -1,2 +1,33 @@
-//hello sdlfkajkfsjg;s
-//jflksj vlf;dsalfksfjasdklfjadsfj ladksjf ;ads jfldajsj
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(NULL);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int stockPrice[1000000];
+  int T_n, D_n;
+  cin>>T_n;
+
+  long long profitSum=0;
+  int maxStock=0;
+
+  for(int i = 0; i<T_n ; ++i){
+    cin>>D_n;
+    for(int day=0; day< D_n; ++day)
+      cin>>stockPrice[day];
+    
+    profitSum=0;
+    maxStock=0;
+    for(int day=D_n -1 ; day>=0 ; --day){
+      if(stockPrice[day] < maxStock)
+        profitSum += (maxStock - stockPrice[day]);
+      else
+        maxStock = stockPrice[day];
+    }
+    cout<<profitSum<<endl;
+  }
+
+  return 0;
+}
--- AlgAndDS/FastInverseSQRT/FISR.c
+++ AlgAndDS/FastInverseSQRT/FISR.c
@@ -13,15 +13,15 @@ float Q_rsqrt(float number)
     i = 0x5F375A86 - (i >> 1); // what the fuck?
     y = *(float*)&i;
     y = y*(threehalfs - (x2*y*y));// 1st iteration
-//  y = y * ( threehalfs - ( x2 * y * y ) ); // 2nd iteration, this can be removed
+    y = y * ( threehalfs - ( x2 * y * y ) ); // 2nd iteration, this can be removed
 
     return y;
 }
 
 int main(){
-    //double ANS = Q_rsqrt(162214242);
-    double ANS = 1/(sqrt(162214242));
-    printf("%lf",ANS);
+    double ANS = Q_rsqrt(1246212343423423342);
+    //double ANS = 1/(sqrt(1246212343423423342));
+    printf("%.99lf",ANS);
 
     return 0;
 }
