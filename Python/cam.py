c7e40bd58ae72c2bcc5ee835ec5d52224fa0b3a6
김스시 <57784879+omakasekim@users.noreply.github.com>
2022-07-24 13:52:06 +0900

    Create cam.py

---
+++ Python/cam.py
@@ -0,0 +1,17 @@
+from cv2 import VideoCapture, resize, flip
+import asciify
+
+cam = VideoCapture(0)
+cam.set(3, 180)
+cam.set(4, 240)
+
+while True:
+
+    s, img = cam.read()
+
+    if s:
+        img = flip(resize(img, (240, 87)), 1)
+        asciify.asciify(img)
+
+cam.release()
+
